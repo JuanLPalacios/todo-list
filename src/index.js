042f55cd8b3cc74ce02ed9e8cadcdba626682867
JuanLP <juan.luis.palacios.p@gmail.com>
2022-02-16 17:18:25 -0500

    Add function to generate list

--- src/index.js
+++ src/index.js
@@ -17,3 +17,16 @@ const todo = [
     index: 2,
   },
 ];
+
+function update() {
+  document.getElementById('list').innerHTML = todo.sort((a, b) => a.index - b.index).map((item, i) => `
+  <li class="todo-li">
+    <input type="checkbox" id="chk-${i}" ${item.completed ? 'checked' : ''}>
+    <label for="chk-${i}"></label>
+    <input type="text" id="txt-${i}" value="${item.description}" >
+    <label for="txt-${i}">${item.description}</label>
+    <span class="handle"></span>
+  </li>`).join('');
+}
+
+update();
